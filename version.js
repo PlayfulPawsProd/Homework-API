3f9ec188e13e3692eabd4e26d211354e6571aa8d
PlayfulPawsProd <PlayfulPawsProductions@gmail.com>
2025-04-09 15:01:04 -0500

    Add files via upload

---
+++ version.js
@@ -0,0 +1,4 @@
+// version.js - Holds the current app version and update notes! Nyaa~!
+
+const APP_VERSION = "1.5.0"; // Change this when you update!
+const APP_UPDATE_NOTE = "Added Mika-Gotchi Poo Incident & About Popup! 💩✨"; // Describe the latest cool thing!
