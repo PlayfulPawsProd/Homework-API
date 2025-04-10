33fc4bf578dc3a2efe99c620e663c468bbfb2077
PlayfulPawsProd <PlayfulPawsProductions@gmail.com>
2025-04-09 21:56:59 -0500

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 // version.js - Holds the current app version and update notes! Nyaa~!
 
-const APP_VERSION = "1.6.1"; // Change this when you update!
+const APP_VERSION = "1.6.2"; // Change this when you update!
 const APP_UPDATE_NOTE = "Added Mika-Gotchi Poo Incident & About Popup! 💩✨ Added chat to Gotchi page! Notifications! Code cleanup!"; // Describe the latest cool thing!
