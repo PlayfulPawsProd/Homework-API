fca88a7a5c4d0d0ba0b94adf61cf5c617b4dacf6
PlayfulPawsProd <PlayfulPawsProductions@gmail.com>
2025-04-20 17:45:37 -0500

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 // version.js - Holds the current app version and update notes! Nyaa~!
 
-const APP_VERSION = "1.7.2"; // Change this when you update!
-const APP_UPDATE_NOTE = "Added Mika-Gotchi Poo Incident & About Popup! 💩✨ Added chat to Gotchi page! Notifications! Code cleanup! Advice/Complaint corner update! Hopefully fixed jetpack game for iPhone!"; // Describe the latest cool thing!
+const APP_VERSION = "1.7.3"; // Change this when you update!
+const APP_UPDATE_NOTE = "Added Mika-Gotchi Poo Incident & About Popup! 💩✨ Added chat to Gotchi page! Notifications! Added Backup/Restore feature!"; // Describe the latest cool thing!
